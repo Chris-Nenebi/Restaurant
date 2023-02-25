6039a816d7ef580e74cfb6dfa4956a24c3891cb5
Chris-Nenebi <chrisnenebi@yahoo.com>
2023-02-25 19:38:08 +0000

    creating serializers,adding some urls and securing the app through authentication

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'reservation',
+     'rest_framework',
+     'rest_framework.authtoken',
+     'djoser',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +148,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#add the following line
+DJOSER={"USER_ID_FIELD":"username"}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
